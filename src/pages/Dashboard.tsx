ee9fb884945bf61282b443faefb24d3f173102d4
Mohamed Loay <126088881+mohamed-elrabany@users.noreply.github.com>
2026-09-24 05:25:30 +0300

    fix: deployment error removed unused imports

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,6 @@
 import Bookmark from "../components/ui/Bookmark";
 import { useLayout } from "../hooks/useLayout";
-// import { useLoaderData } from "react-router";
 import { useAppSelector } from "../store/hooks";
-import { fetchBookmarks } from "../services/bookmarkServices";
-// import {bookmarks} from "../types/bookmark";
 
 export function Component() {
     const { layout } = useLayout();
